d5b2b4332b550ff622abae0e799b1693463cecf3
SergioLopezBailon <47179066+SergioLopezBailon@users.noreply.github.com>
2020-03-23 12:05:59 +0100

    Segundo commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,7 +18,7 @@
         <a href="#" class="btn btn-outline-light btn-lg">Register</a>
     </div>
     <div class="alert alert-warning alert-dismissible fade show" role="alert">
-        <p>Si continuas navegando por esta página web aceptas nuestras cookies y que eres mayor de 18 años.</p>
+        <p>Si continuas navegando por esta página web aceptas nuestras cookies y que eres mayor de asdfasd 18 años.</p>
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
         </button>
